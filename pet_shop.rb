7740ad3376d954414bdcda6262b874105129f588
Becky <rebeccajoynielsen@gmail.come>
2019-03-02 20:30:16 +0000

    test 21 complete

--- pet_shop.rb
+++ pet_shop.rb
@@ -76,9 +76,14 @@ def customer_can_afford_pet(customer, new_pet)
   return false
 end
 
-def sell_pet_to_customer(pet_shop, pet, customer)
-  customer[:pets].push(pet)
-  pet_shop[:admin][:pets_sold] += 1
-  customer[:cash] -= pet[:price]
-  pet_shop[:admin][:total_cash] += pet[:price]
+def sell_pet_to_customer(pet_shop, pet_wanted, customer)
+  for pet in pet_shop[:pets]
+    if pet == pet_wanted
+      customer[:pets].push(pet)
+      pet_shop[:admin][:pets_sold] += 1
+      customer[:cash] -= pet[:price]
+      pet_shop[:admin][:total_cash] += pet[:price]
+    end
+  end
+  return nil
 end
